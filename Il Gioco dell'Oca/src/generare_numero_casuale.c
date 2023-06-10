4051ff319c182c31a34a35e1433f6aca7ea65d86
B4RTM0SS <potitoflorio@outlook.it>
2023-06-10 18:28:42 +0200

    Modifiche 10.06.2023 - 18.28

--- Il Gioco dell'Oca/src/generare_numero_casuale.c
+++ Il Gioco dell'Oca/src/generare_numero_casuale.c
@@ -12,7 +12,7 @@
 void caricare_seme (long long * seme);
 long long generare_seme (long long seme);
 void scrivere_seme (long long * seme);
-long int generare_casuale (int seme, int numero_massimo, int numero_minimo);
+long int generare_casuale (long int seme, int numero_massimo, int numero_minimo);
 void creare_file_seme ();
 
 
@@ -59,7 +59,7 @@ void scrivere_seme (long long * seme) {
 
 
 
-long int generare_casuale (int seme, int numero_massimo, int numero_minimo) {
+long int generare_casuale (long int seme, int numero_massimo, int numero_minimo) {
     long int numero_casuale = (seme % numero_massimo) + numero_minimo;
     return numero_casuale;
 }
@@ -68,7 +68,7 @@ long int generare_casuale (int seme, int numero_massimo, int numero_minimo) {
 
 void creare_file_seme () {
     FILE * file_seme = fopen(FILE_SEME, "wb");
-    fwrite((const long long *) SEME_INIZIALE, sizeof(int), 1, file_seme);
+    fwrite(&SEME_INIZIALE, sizeof(long long), 1, file_seme);
     fclose(file_seme);
     return;
 }
